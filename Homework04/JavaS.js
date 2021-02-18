23cb76105649699703ea1412e97d79500899194f
Nate Hogue <nhogue@missouriwestern.edu>
2021-02-17 19:38:48 -0600

    Completed homework 04

--- Homework04/JavaS.js
+++ Homework04/JavaS.js
@@ -1,16 +1,41 @@
-window.addEventListener("load", function(){
-    var data = ["doge", "cate", "birb", "doggo", "moon moon", "awkward seal"];
-    
-    var perrow = 5,
-        html = '<table class="table"><tr><tr><tr><tr><tr>';
-    for (var i=0; i<data.length; i++) {
-      html += `<td>${data[i]}</td>`;
-      var next = i+1;
-      if (next%perrow==0 && next!=data.length) {
-        html += "</tr><tr>";
-      }
-    }
-    html += "</tr></table>";
-  
-    document.getElementById("container").innerHTML = html;
-  });
+function showClasses(){
+    courses.forEach( a => {
+        var tablename=""
+        switch(a.course.substring(0,3)){
+            case "CSC" :
+                tablename= "Csc"
+                break;
+            case "ACT" :
+                tablename= "Act"
+                break;
+            case "MAT" :
+                tablename= "Mat"
+                break;
+            case "PHY" :
+                tablename= "Phy"
+                break;
+        }
+        
+        var table = document.querySelector("#"+tablename);
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(-1).innerHTML=a.course;
+        var cell2 = row.insertCell(-1).innerHTML=a.crn;
+        var cell3 = row.insertCell(-1).innerHTML=a.title;
+        
+        
+        var dates = "Days: "+a["days"]+" Time: "+ a["times"]
+        
+        if(a["times"]=="-"){
+            dates="Online"
+        }
+        var cell4 = row.insertCell(-1).innerHTML=dates;
+        var cell5 = row.insertCell(-1).innerHTML=a.instructor;
+        
+        if(a["hours"]==0){
+            row.classList.add("bg-warning");      
+        }
+        table.classList.remove("table-striped")
+        table.classList.add("table-striped")
+    })
+
+}
