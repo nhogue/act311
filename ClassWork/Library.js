e891d8b09b0c698a3d26dca0c9a2fc1ae472399b
Nate Hogue <nhogue@missouriwestern.edu>
2021-02-23 19:37:13 -0600

    'Test'

---
+++ ClassWork/Library.js
@@ -0,0 +1,8 @@
+const library = [
+    "The Way of Kings",
+    "Words of Radiance",
+    "Oathbringer",
+    "Rhythm of War"
+]
+
+library.forEach(element => console.log(element));
---
+++ README.md
@@ -0,0 +1,1 @@
+Homework for ACT311
