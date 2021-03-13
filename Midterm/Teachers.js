db3df609871b2a00266c91230e701143b6d11f6f
Nate Hogue <nhogue@missouriwestern.edu>
2021-03-13 17:15:53 -0600

    Partially completed step 3, finished step 4, partially completed step 5

--- Midterm/Teachers.js
+++ Midterm/Teachers.js
@@ -1,5 +1,6 @@
-let url = 'https://raw.githubusercontent.com/nhogue/act311/main/CSMP.json';  
+let url = 'https://raw.githubusercontent.com/nhogue/act311/main/midterm/'; 
 let allURL = 'https://raw.githubusercontent.com/nhogue/act311/main/midterm/all.json';
+let URLselected = '';
 function departments() {
 fetch(allURL)    
     .then(getResponse1)
@@ -16,64 +17,36 @@ function getData1(data) {
     console.log(data);
     var departmentArray = [];
     for(dept of data){
-        if(!departmentArray.includes(dept["dept"])){
-            departmentArray.push(dept["dept"]);
+        if(!departmentArray.includes(dept)){
+            departmentArray.push(dept);
         }
     }
-    var select1 = document.getElementById("DepartmentSelection");
+    var select1 = document.getElementById("DepartmentSelect");
     for (a of departmentArray){
         var option1 = document.createElement("option");
-        option1.text = a;
-        option1.id = a;
+        option1.text = a["dept"];
+        option1.id = a["filename"];
+        console.log(option1);
         select1.add(option1);
     }
 }
-    function DepartmentSelection(button) {
-        var selected = button.options[button.selectedIndex].id;
-        fetch(allURL)
-        .then(getResponse)
-        .then(data =>{
-            getDepts(selected, data);
-        })
-        .catch("Error...");
-    }
+function DepartmentSelection(button) {
+    console.log(button);
+    var selected1 = button.options[button.selectedIndex].id;
+    console.log(selected1);
+    URLselected = selected1;
+    instructors();
     
-    function getDepts(selected, data) {
-       var deptArray = [];
-       for(b of data){
-           if(selected==b["dept"]){
-            deptArray.push(b);
-        }
-       }
-       courseArray = classSort(courseArray);
-       PrintHTML1(courseArray, selected);
     }
-    function PrintHTML1(arr, instructor) {
-        var grid = document.getElementById("theList");
-        grid.innerHTML = "";
-        var div = document.createElement('div');
-        div.className = "col text-center bg-dark";
-        div.appendChild(document.createTextNode(instructor));
-        grid.appendChild(div);
-    
-        for (y of arr){
-            div = document.createElement('div');
-            var string = y.course+"   "+y.days+"   "+y.times;
-            div.className = "col text-center bg-dark";
-            div.appendChild(document.createTextNode(string));
-            grid.appendChild(div);
-            grid.classList.add("border");
-        }
-    }
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 function instructors() {
     setStatus("Start Fetch", "text-white bg-danger");
 
-fetch(url)
-    .then(getResponse)
-    .then(getData)
-    .catch("Error...")
-    ;
+    fetch(url + URLselected)
+        .then(getResponse)
+        .then(getData)
+        .catch("Error...")
+        ;
 }
 function getResponse(response) {
     console.log(response);
@@ -92,6 +65,7 @@ function getData(data) {
     }
 
     var select = document.getElementById("InstructorSelect");
+    select.innerHTML='';
     for(x of instructorArray){
         var option = document.createElement("option");
         option.text = x;
@@ -114,7 +88,7 @@ function TeacherSelection(button) {
     //console.log(button);
     var selected = button.options[button.selectedIndex].id;
     //console.log(selected);
-    fetch(url)
+    fetch(url + URLselected)
     .then(getResponse)
     .then(data =>{
         getClasses(selected, data);
@@ -147,13 +121,22 @@ function PrintHTML(arr, instructor) {
 
     for (y of arr){
         div = document.createElement('div');
-        var string = y.course+"   "+y.days+"   "+y.times;
+        var stringCourse = y.course
+        var stringDay = "   "+y.days
+        var stringTime ="   "+y.times;
         div.className = "col text-center bg-dark";
-        div.appendChild(document.createTextNode(string))
+        div.appendChild(document.createTextNode("Name: "))
+        div.appendChild(document.createTextNode(stringCourse))
+        div.appendChild(document.createTextNode(" Day: "))
+        div.appendChild(document.createTextNode(stringDay))
+        div.appendChild(document.createTextNode(" Time: "))
+        div.appendChild(document.createTextNode(stringTime))
         grid.appendChild(div);
         grid.classList.add("border");
+        grid.classList.add("p-0");
     }
 }
+
 function classSort(arr) {
     arr.sort(function (a,b) {
         console.log(a,b)
